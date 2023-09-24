a89ea82f372bc04c4b1f992a65e1ba4e18f047c9
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-09-24 23:02:07 +0530

    npm and node install

--- main.sh
+++ main.sh
@@ -2,13 +2,33 @@
 
 apt update -y
 apt install -y 
+
 apt install software-properties-common  -y 
+
 apt install curl -y 
+
 aapt install pt-transport-https -y
+
 apt install wget -y
+
 apt install sudo -y
+
 apt-cache search htop
+
 apt install htop -y
+
 apt install apache2 -y
+
+apt install nodejs
+echo "Node is successfully Installed\n"
+echo "Node Version\n"
+node --version
+
+apt install npm
+echo "Npm is successfully Installed\n"
+echo "Npm Version"
+npm --version
+npm i npm@latest -g
+
 apt install clean 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
